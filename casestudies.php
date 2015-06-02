0c1fdf80eba43030b1cb35490dcf8a27ff931c39
David Rhoden <david@davidrhoden.com>
2015-06-02 07:40:12 -0500

    initial commit

---
+++ casestudies.php
@@ -0,0 +1,55 @@
+<div class="study">
+American Express Travel
+David: "This was a giant job where I was brought in in the middle. The first ting I noticed is that the hotel, car, and flight booking all had wildly different code, yet we, as front-end developers , were supposed to make it all look the same."
+Tech: CSS/HTML, Ruby on Rails, Photoshop
+</div>
+
+<div class="study">
+AT&amp;T: The Innovators
+David: "This was mostly about communiating with the designer. They were used to using Flash and building sites with a lot of bells-and whistles 'wow' factor. My job was to implement and already-approveddesign in a way that it would work on a phone.""
+Tech: CSS/HTML, Ruby on Rails, Photoshop
+</div>
+
+
+<div class="study">
+Bajibot
+David: "This was mostly about communicating with the designer. They were used to using Flash and building sites with a lot of bells-and whistles 'wow' factor. My job was to implement an already-approved design in a way that it would work on a phone."
+Tech: CSS/HTML, Javascript/jQuery, Ajax, HTML5 Video, Video fallbacks, Wordpress
+</div>
+
+<div class="study">
+Crown Royal
+David: "On this one, we had to implement the whims of a rogue creative director who eventually got fired. He actually wanted the whole site background to look like moving, flowing liquor. He had a lot of crazy requests that the client loved, and we managed to implement most of them. It's such a ridiculous, tacky site, but I kind of love it."
+Tech: Javascript/jQuery, CSS/HTML, Photoshop
+</div>
+
+<div class="study">
+Kid Rock for Devil's Cut Bourbon
+David: "This was a challenging but silly and fun job. What I ad to do was set up a page that would automatically style Kid Rock's Tweets regardless of how long or short they were. I had to do this with Cufón, a sIFR alternative I hadn't worked with before. (I had worked with sIFR some, and that was no fun either.)
+Tech: CSS/HTML, Cufón, Flash, Photoshop, Jenkins
+</div>
+
+<div class="study">
+Smirnoff Vodka
+</div>
+
+<div class="study">
+The Frisky
+</div>
+
+<div class="study">
+Jawbone Bluetooth Earpieces and Speakers
+</div>
+
+<div class="study">
+IAC Homepage
+</div>
+
+<div class="study">
+Wordpress
+<ul>
+<li>Sick To Death!</li>
+<li>EHC Health Care</li>
+<li>DST herapists</li>
+</ul>
+</div>
---
+++ index.php
@@ -0,0 +1,24 @@
+HAPPENING.
+
+[big words]
+We are a mulit-disciplinary....
+wait, you already lost interest when you read "multi-disciplinary", right?
+
+We'll try to skip the buzzwords.
+
+We are a talented team of developers who can help you do and build the things you want to see on the web.
+
+What have we done?
+
+We're not limited by what's been done before.
+
+We're weirdos.
+
+iOS development. (ALEC)
+
+Great design. (DAN) 
+
+experience with web audiences (DAVID)
+
+
+
